0cc9bf4bf9a82d72501a7237d0cd5d3d51af44a0
mayapony <mayapony@foxmail.com>
2025-02-18 21:46:35 +0800

    ✨ 测试导入Anki及翻译

--- app/(tabs)/reader.tsx
+++ app/(tabs)/reader.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
-import { useWindowDimensions } from "react-native";
+import {
+  Button,
+  Text,
+  ToastAndroid,
+  useWindowDimensions,
+  View,
+} from "react-native";
 import {
   Annotation,
   Reader,
@@ -9,6 +15,10 @@ import {
 import { useFileSystem } from "@epubjs-react-native/expo-file-system";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ExpoAnkiDroidApiModule from "@/modules/expo-ankidroid-api";
+import * as Notifications from "expo-notifications";
+import axios from "axios";
+import md5 from "crypto-js/md5";
 
 function Book() {
   const { width, height } = useWindowDimensions();
@@ -19,14 +29,15 @@ function Book() {
     Annotation | undefined
   >(undefined);
   const [tempMark, setTempMark] = React.useState<Annotation | null>(null);
-
   const annotationsListRef = React.useRef<BottomSheetModal>(null);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Reader
         src="https://s3.amazonaws.com/moby-dick/OPS/package.opf"
         width={width}
         height={height * 0.85}
+        // @ts-ignore
         fileSystem={useFileSystem}
         initialLocation="introduction_001.xhtml"
         initialAnnotations={[
@@ -85,10 +96,49 @@ function Book() {
             },
           },
           {
-            label: "Add Note",
+            label: "To Anki",
             action: (cfiRange, text) => {
-              addAnnotation("highlight", cfiRange, { isTemp: true });
-              annotationsListRef.current?.present();
+              console.log("Add Note", cfiRange, text);
+              const params = {
+                q: text,
+                from: "auto",
+                to: "zh",
+                appid: "",
+                salt: "123456",
+                get sign() {
+                  return md5(this.appid + this.q + this.salt + "");
+                },
+              };
+              console.log({ params });
+              axios({
+                method: "post",
+                url: "https://fanyi-api.baidu.com/api/trans/vip/translate",
+                params: params,
+              })
+                .then((response) => {
+                  const transResult = response.data?.trans_result?.[0]?.dst;
+                  if (!transResult) {
+                    throw new Error("Translation Failed");
+                  }
+                  const card = {
+                    Front: text,
+                    Back: transResult,
+                  };
+                  console.log({ card });
+                  ExpoAnkiDroidApiModule.addCardsToAnkiDroidAsync([card])
+                    .then((added) => {
+                      console.log(added);
+                      ToastAndroid.show("Card Added", ToastAndroid.SHORT);
+                    })
+                    .catch((error) => {
+                      console.log(error);
+                      ToastAndroid.show("Card Add Failed", ToastAndroid.SHORT);
+                    });
+                })
+                .catch((error) => {
+                  console.log(error);
+                  ToastAndroid.show("Translation Failed", ToastAndroid.SHORT);
+                });
               return true;
             },
           },
@@ -99,9 +149,37 @@ function Book() {
 }
 
 export default function ReaderScreen() {
+  const [isApiAvailable, setIsApiAvailable] = React.useState<string>("");
+
+  React.useEffect(() => {
+    Notifications.requestPermissionsAsync();
+  }, []);
+
   return (
     <ReaderProvider>
       <Book />
+      <View>
+        <Text>API有效性检查: {isApiAvailable}</Text>
+        <Button
+          title="Add Card"
+          onPress={() => {
+            const test = {
+              Front: Math.random().toString(36).substring(7),
+              Back: Math.random().toString(36).substring(7),
+            };
+
+            ExpoAnkiDroidApiModule.addCardsToAnkiDroidAsync([test])
+              .then((added) => {
+                console.log(added);
+                ToastAndroid.show("Card Added", ToastAndroid.SHORT);
+              })
+              .catch((error) => {
+                console.log(error);
+                ToastAndroid.show("Card Add Failed", ToastAndroid.SHORT);
+              });
+          }}
+        />
+      </View>
     </ReaderProvider>
   );
 }
